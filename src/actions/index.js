7ad8b5c6ea126783d78aee66b1f01c8ce1c7ab31
Ian Lin <ian.lin@ilin-mac.local>
2018-08-29 09:54:35 -0700

    Fix Typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API_KEY = '644570e8f6fe4313599437e937dd75cd';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchweather(city){
+export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
 
